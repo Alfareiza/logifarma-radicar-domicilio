4428450983aafcbfa6dd53ad0138fb2035b5d76f
alfonso <alfareiza@gmail.com>
2022-11-12 20:06:19 -0300

    configuración inicial de whitenoise

--- core/settings.py
+++ core/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -131,3 +132,7 @@ EMAIL_USE_TLS = config('EMAIL_USE_TLS')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'tmp'
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
