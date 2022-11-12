fbd6724f8d1666dbca3e3bc92a3b9d87997fdbb7
alfonso <alfareiza@gmail.com>
2022-11-12 12:23:13 -0300

    Relleno de campos para Barrio, Municipio e email para la última vista

--- core/apps/base/forms.py
+++ core/apps/base/forms.py
@@ -5,6 +5,7 @@ from django import forms
 
 from core.apps.base.resources.api_calls import call_api_eps
 
+
 class Home(forms.Form):
     """
     Vista 1: Página inicial de la aplicación
@@ -31,7 +32,7 @@ class AutorizacionServicio(forms.Form):
     num_autorizacion = forms.IntegerField(min_value=100_000)
 
     def clean_num_autorizacion(self):
-        # PRIMERA API
+        # ====== PRIMERA API ======
 
         # resp_api1 = call_api_eps(self.cleaned_data.get('num_autorizacion'))
 
@@ -53,7 +54,8 @@ class AutorizacionServicio(forms.Form):
         #     raise forms.ValidationError("Esta autorización se encuentra vencida.")
         resp_api1['NUMERO_AUTORIZACION'] = self.cleaned_data.get('num_autorizacion')
         return resp_api1
-        # SEGUNDA API
+
+        # ====== SEGUNDA API ======
         # resp_api1 = {"error": "No se han encontrado registros."}
 
 
@@ -67,7 +69,8 @@ class FotoFormulaMedica(forms.Form):
     src = forms.CharField(label='Foto')
 
     def clean_src(self):
-        import base64; from django.core.files.base import ContentFile
+        import base64;
+        from django.core.files.base import ContentFile
         formato, imgstr = self.cleaned_data.get('src').split(';base64,')
         ext = formato.split('/')[-1]
         return ContentFile(base64.b64decode(imgstr), name=f'formula_medica.{ext}')
@@ -84,14 +87,44 @@ class EligeMunicipio(forms.Form):
     """
     Vista 6:
     """
-    ...
+    MUNICIPIOS = (
+        ("0", "Escoja un municipio"),
+        ("1", "Barranquilla, Atlantico"),
+        ("2", "Soledad, Atlantico"),
+        ("3", "Malambo, Atlantico"),
+        ("4", "Baranoa, Atlantico"),
+        ("5", "Galapa, Atlantico"),
+    )
+
+    municipio = forms.ChoiceField(choices=MUNICIPIOS)
+
+    def clean_municipio(self):
+        municipio = self.cleaned_data.get('municipio')
+        if municipio == '0':
+            raise forms.ValidationError("Escoja un municipio antes de continuar")
+        return self.MUNICIPIOS[int(municipio)][1]
 
 
 class EligeBarrio(forms.Form):
     """
     Vista 7:
     """
-    ...
+    BARRIOS = (
+        ("0", "Escoja un barrio"),
+        ("1", "El Valle, Atlantico"),
+        ("2", "Barrio 2"),
+        ("3", "Barrio 3"),
+        ("4", "Barrio 4"),
+        ("5", "Barrio 5"),
+    )
+
+    barrio = forms.ChoiceField(choices=BARRIOS)
+
+    def clean_barrio(self):
+        barrio = self.cleaned_data.get('barrio')
+        if barrio == '0':
+            raise forms.ValidationError("Escoja un barrio antes de continuar")
+        return self.BARRIOS[int(barrio)][1]
 
 
 class DigitaDireccion(forms.Form):
@@ -106,3 +139,4 @@ class DigitaCelular(forms.Form):
     Vista 9:
     """
     celular = forms.IntegerField()
+    email = forms.EmailField(required=False)
