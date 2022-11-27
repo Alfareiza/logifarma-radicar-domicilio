c23523d37ca45cf2833ad00d2d18493c29997361
alfonso <alfareiza@gmail.com>
2022-11-27 15:40:08 -0300

    Implementando la inserción de barrios y municipios vía admin

--- core/apps/base/admin.py
+++ core/apps/base/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.apps.base.models import Municipio, Barrio
+
+
+# Documentation
+# https://docs.djangoproject.com/en/4.1/ref/contrib/admin/
+
+@admin.register(Municipio)
+class MunicipioAdmin(admin.ModelAdmin):
+    list_display = ('name', 'departamento')
+    list_filter = ('departamento',)
+    search_fields = ('name',)
+    ordering = ('name', 'departamento')
+
+
+@admin.register(Barrio)
+class BarrioAdmin(admin.ModelAdmin):
+    list_display = ('name', 'zona', 'cod_zona', 'municipio')
+    list_filter = ('municipio', 'zona', 'status')
+    search_fields = ('name',)
+    ordering = ('municipio', 'name', 'zona')
+    raw_id_fields = ('municipio',)
