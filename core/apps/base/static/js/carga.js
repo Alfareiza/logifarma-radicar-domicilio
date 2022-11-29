c632480e4c6dbf8dfb15842a0b3969f2714bca56
Elias Camacho <eliascamachod@gmail.com>
2022-11-29 15:45:44 -0500

    finalizacion animacion del boton y vista cargando

--- core/apps/base/static/js/carga.js
+++ core/apps/base/static/js/carga.js
@@ -8,12 +8,3 @@ function cargar() {
     carga.style.display = "block";
 };
 
-
-const carga_ds = document.getElementById("load_a") ;
-
-document.getElementById("btn_final").addEventListener("click", cargad);
-
-function cargad() {
-    carga_ds.style.opacity = "1";
-    carga_ds.style.visibility = "visible";
-}
