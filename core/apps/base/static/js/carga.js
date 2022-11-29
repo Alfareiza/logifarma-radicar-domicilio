1cdef6dc49918104a89d4d77313ccff7632b50cd
AlfonsoAreizaG <63620799+Alfareiza@users.noreply.github.com>
2022-11-29 14:33:55 -0300

    Revert "correccion de animacion del boton cargando"
    This reverts commit 67a5e1a786c62a75c0e22410173df7912a8dae37.

--- core/apps/base/static/js/carga.js
+++ core/apps/base/static/js/carga.js
@@ -1,19 +1,17 @@
 
-const carga = document.getElementById("btn_carga") ;
+const carga = document.getElementById('btn_carga') ;
 
-document.getElementById("btn_conti").addEventListener("click", cargar);
+document.getElementById('btn_conti').addEventListener("click", cargar);
 
 function cargar() {
-    
     carga.style.display = "block";
-};
+}
 
+const carga_ds = document.getElementById('load_a') ;
 
-const carga_ds = document.getElementById("load_a") ;
+document.getElementById('btn_btn_final').addEventListener("click", cargadd);
 
-document.getElementById("btn_final").addEventListener("click", cargad);
-
-function cargad() {
+function cargadd() {
     carga_ds.style.opacity = "1";
     carga_ds.style.visibility = "visible";
 }
