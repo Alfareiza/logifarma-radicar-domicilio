e76340d76bd28f4eb4d46fb95f6610ab54055bbb
AlfonsoAreizaG <63620799+Alfareiza@users.noreply.github.com>
2022-12-01 18:47:20 -0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -139,6 +139,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'tmp'
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = BASE_DIR / "static"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 logger = logging.getLogger('django')
