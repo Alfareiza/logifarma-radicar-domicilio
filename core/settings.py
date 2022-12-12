9344b0c4df8669beee7f97140663376908405107
alfonso <alfareiza@gmail.com>
2022-12-12 20:08:17 -0300

    Alterado para timezone de Bogota

--- core/settings.py
+++ core/settings.py
@@ -112,11 +112,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'es-co'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Bogota'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
