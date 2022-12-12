3080e4d551cbd4d97058b99bd8c9ef9a22ec12cd
alfonso <alfareiza@gmail.com>
2022-12-12 20:07:37 -0300

    Modelo para radicación

--- core/apps/base/models.py
+++ core/apps/base/models.py
@@ -1,9 +1,15 @@
 from django.db.models import (
     CASCADE,
     CharField,
+    DateTimeField,
+    EmailField,
     ForeignKey,
+    GenericIPAddressField,
     IntegerField,
+    JSONField,
     Model,
+    PositiveBigIntegerField,
+    PositiveIntegerField,
 )
 
 
@@ -21,8 +27,8 @@ class Municipio(Model):
 
 class Barrio(Model):
     name = CharField(max_length=128)
-    zona = CharField(max_length=20)
-    municipio = ForeignKey(Municipio, on_delete=CASCADE)
+    zona = CharField(max_length=20, blank=False)
+    municipio = ForeignKey(Municipio, blank=False, on_delete=CASCADE)
     cod_zona = IntegerField()
     status = IntegerField()
 
@@ -32,3 +38,36 @@ class Barrio(Model):
 
     def __str__(self):
         return f"{self.name.title()} - Zona {self.zona.title()}"
+
+
+class Radicacion(Model):
+    datetime = DateTimeField(auto_now_add=True)
+    numero = PositiveIntegerField(unique=True)
+    municipio = ForeignKey(Municipio, on_delete=CASCADE)
+    barrio = ForeignKey(Barrio, on_delete=CASCADE)
+    celular_uno = PositiveBigIntegerField()
+    celular_dos = PositiveBigIntegerField()
+    email = EmailField(max_length=254)
+    direccion = CharField(max_length=150)
+    ip = GenericIPAddressField(protocol='both')
+
+    # Campos de Paciente
+    paciente_nombre = CharField(max_length=150)
+    paciente_cedula = PositiveIntegerField()
+    paciente_data = JSONField()
+
+    # Campos de Domiciliario
+    domiciliario_nombre = CharField(max_length=150, blank=True, null=True)
+    domiciliario_identificacion = PositiveIntegerField(blank=True, null=True)
+    domiciliario_empresa = CharField(max_length=150, blank=True, null=True)
+
+    # Campos adicionales
+    estado = CharField(max_length=64, blank=True, null=True)
+    alistamiento = DateTimeField(blank=True, null=True)
+    alistado_por = CharField(max_length=150, blank=True, null=True)
+    despachado = DateTimeField(blank=True, null=True)
+    acta_entrega = CharField(max_length=150, blank=True, null=True)
+    factura = CharField(max_length=150, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.numero}"
