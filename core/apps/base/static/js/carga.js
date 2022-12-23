2d98a68e78e5f82da93779718dc11deb4443f1b5
Elias Camacho <eliascamachod@gmail.com>
2022-12-23 10:31:19 -0500

    validacion del correo y busqueda en barrio y municipio

--- core/apps/base/static/js/carga.js
+++ core/apps/base/static/js/carga.js
@@ -1,10 +1,48 @@
+const carga = document.getElementById("btn_carga") ;
 
+function cargar2() {
 
-function cargar() {
-    let carga = document.getElementById("btn_carga") ;
-    
+    /* let inp = document.getElementById('id_digitaDireccionBarrio-direccion').addEventListener("keyup"); */
+
+    if ($('.select_opt').is(':checked')) {
+        
+    if($('#id_digitaDireccionBarrio-direccion').val().length != 0){
+
         carga.style.display = "block";
+
+    }else{
+        carga.style.display = "none";
+    }
+}
+
+};
+
+function cargar3() {
+    let numvali = document.getElementById("id_digitaCelular-celular");
+
+    if (numvali.value < 1000000000) {
+        
+        carga.style.display = "none";
+    }else{
+        carga.style.display = "block";
+    }
+};
+
+function cargar4() {
     
+    carga.style.display = "block";
+
+};
+
+function cargar5() {
+    /* let selecheck = document.getElementsByClassName("select_opt"); */
+
+    if ($('.select_opt').is(':checked') ) {
+        
+        carga.style.display = "block";
+    }else{
+        carga.style.display = "none";
+    }
 };
 
 
--- core/apps/base/static/js/carga_final.js
+++ core/apps/base/static/js/carga_final.js
@@ -1,15 +1,57 @@
-
 const carga_ds = document.getElementById("load_a") ;
+const correo = document.getElementById("id_digitaCorreo-email");
+const regExp = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-4])?\.)+[a-z0-4](?:[a-z0-4-]*[a-z0-4])?$/;
+
+const icon1 = document.querySelector(".icon1");
+const icon2 = document.querySelector(".icon2");
+const error = document.querySelector(".text-error");
 
 document.getElementById("btn_final").addEventListener("click", cargad);
 
+function corrvali() {
+    if (correo.value.match(regExp)) {
+        correo.style.borderColor="#27ae60";
+        correo.style.background="#eafaf1";
+        icon1.style.display ="none";
+        icon2.style.display ="block";
+        error.style.display ="none";
+    }else{
+        correo.style.borderColor="#e74c3c";
+        correo.style.background="#fceae9";
+        icon1.style.display ="block";
+        icon2.style.display ="none";
+        error.style.display ="block";
+    }
+
+    if (correo.value=="") {
+        correo.style.borderColor="lightgrey";
+        correo.style.background ="#fff";
+        icon1.style.display ="none";
+        icon2.style.display ="none";
+        error.style.display ="none";
+    }
+};
+
+function letra(e) {
+    key=e.keyCode || e.which;
+    teclado = String.fromCharCode(key).toLowerCase();
+    letras =" abcdefghijklmnñopqrstuvwxyz";
+    especiales = "8-37-38-46-164";
+    teclado_especial = false;
+    for(var i in especiales){
+        if (key==especiales[i]) {
+            teclado_especial=true;break;
+        }
+        if (letras.indexOf(teclado)==-1 && !teclado_especial) {
+            return false;
+        }
+    }
+};
+
 function cargad() {
-    
-let campo = document.getElementById("id_digitaCorreo-email");
-let regExp = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-4])?\.)+[a-z0-4](?:[a-z0-4-]*[a-z0-4])?$/;
 
-    if (campo.value.match(regExp)) {
-
+    if (correo.value.match(regExp)) {
+ 
         carga_ds.style.opacity = "1";
         carga_ds.style.visibility = "visible";
     } else {
@@ -21,7 +63,7 @@ let regExp = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(
 };
 
 
-let imgLogoElement = document.getElementById("logo-loader");
+const imgLogoElement = document.getElementById("logo-loader");
 Loadgo.init(imgLogoElement);
 
 Loadgo.loop(document.getElementById('logo-loader'), 10);
