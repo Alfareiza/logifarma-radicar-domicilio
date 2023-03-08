c5e146b2bae2ad583e4033da398bcae1d05dbcd1
alfonso <alfareiza@gmail.com>
2023-03-08 14:44:35 -0300

    Excluyendo vistas y urls de pruebas

--- core/apps/base/urls.py
+++ core/apps/base/urls.py
@@ -1,15 +1,10 @@
-"""URL mappings for the base app (transaction app)"""
-
 from django.urls import path
 
-from core.apps.base.spark import Spark, finalizado_spark
 from core.apps.base.views import ContactWizard, finalizado
 
 app_name = 'base'
 
 urlpatterns = [
     path('', ContactWizard.as_view(), name='home'),
-    path('finalizado/', finalizado, name='done'),
-    path('finalizado_spark/', finalizado_spark, name='done_spark'),
-    path('spark/', Spark.as_view(), name='spark'),
+    path('finalizado/', finalizado, name='done')
 ]
