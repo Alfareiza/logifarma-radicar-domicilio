fea2650aa01d153ab678e6cf4274b549af780fac
Elias Camacho <eliascamachod@gmail.com>
2023-03-16 15:34:46 -0500

    correccion en la validacion de correo

--- core/apps/base/static/js/carga_final.js
+++ core/apps/base/static/js/carga_final.js
@@ -7,33 +7,37 @@ const error = document.querySelector(".text-error");
 emailInput.addEventListener('input', () => {
   const emails = emailInput.value.split(/,\s*/);
   const validEmails = [];
+  const regExp =/^[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-4])?\.)+[a-zA-Z0-4](?:[a-zA-Z0-4-]*[a-zA-Z0-4])?$/;
 
   for (const email of emails) {
-    if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+    if (regExp.test(email)) {
       validEmails.push(email);
     }
-  }
+  } 
 
-  if (validEmails.length === 0) {
-    emailInput.style.borderColor="lightgrey";
-    emailInput.style.background ="#fff";
-        icon1.style.display ="none";
-        icon2.style.display ="none";
-        error.style.display ="none";
-  } else if (validEmails.length === emails.length) {
-    emailInput.style.borderColor="#27ae60";
-    emailInput.style.background="#eafaf1";
+  if (validEmails.length === emails.length) {
+        emailInput.style.borderColor="#27ae60";
+        emailInput.style.background="#eafaf1";
         icon1.style.display ="none";
         icon2.style.display ="block";
         error.style.display ="none";
   } else {
-    emailInput.style.borderColor="#e74c3c";
-    emailInput.style.background="#fceae9";
+        emailInput.style.borderColor="#e74c3c";
+        emailInput.style.background="#fceae9";
         icon1.style.display ="block";
         icon2.style.display ="none";
         error.style.display ="block";
   }
+  if (emailInput.value=="") {
+    emailInput.style.borderColor="lightgrey";
+    emailInput.style.background ="#fff";
+    icon1.style.display ="none";
+    icon2.style.display ="none";
+    error.style.display ="none";
+    
+  }
 });
+
 
 
 document.getElementById("btn_final").addEventListener("click", cargad);
