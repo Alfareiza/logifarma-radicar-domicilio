62355ce2940c077f2c66a3e94ccc8a60ccdc182c
Alfonso <alfareiza@gmail.com>
2023-11-09 18:30:06 -0300

    Creada clase para tratar todo lo relacionado con Emails

--- core/apps/base/resources/email_helpers.py
+++ core/apps/base/resources/email_helpers.py
@@ -4,8 +4,12 @@ import DNS
 import socket
 
 from decouple import config, Csv
+from django.conf import settings
+from django.core.mail import EmailMessage
+from django.template.loader import get_template
 
-from core.settings import logger
+from core.apps.base.resources.tools import notify, convert_bytes
+from core.settings import logger, BASE_DIR
 
 
 def get_complement_subject(payload: dict) -> str:
@@ -43,7 +47,8 @@ def make_subject_and_cco(info_email) -> tuple:
         subject = f"{info_email['NUMERO_AUTORIZACION']} - Este es el " \
                   f"número de radicación de tu domicilio en Logifarma"
 
-    if info_email.get('documento') in ('CC99999999', ) or info_email.get('NUMERO_AUTORIZACION') in (99_999_999, 99_999_998):
+    if info_email.get('documento') in ('CC99999999',) or info_email.get('NUMERO_AUTORIZACION') in (
+            99_999_999, 99_999_998):
         subject = '[OMITIR] CORREO DE PRUEBA'
         with contextlib.suppress(Exception):
             copia_oculta.remove('radicacion.domicilios@logifarma.co')
@@ -132,3 +137,114 @@ def validar_email(email, debug=False):
 
 
 ServerError = DNS.ServerError
+
+
+class Email:
+    def __init__(self, foto=None, template=BASE_DIR / "core/apps/base/templates/notifiers/correo_sin_autorizacion.html"):
+        self.log_text = None
+        self.foto = foto
+        self.template = get_template(template)
+
+    def prepare_mail(self, info: dict) -> EmailMessage:
+        subject, copia_oculta = self.make_subject_and_cco(info)
+        destinatary = self.make_destinatary(info)
+        html_content = self.template.render(info)
+        email = EmailMessage(
+            subject, html_content, to=destinatary, bcc=copia_oculta,
+            from_email=f"Domicilios Logifarma <{settings.EMAIL_HOST_USER}>"
+        )
+        email.content_subtype = "html"
+
+        if self.foto:
+            # email.attach(self.foto_fmedica.name, self.foto_fmedica.new_file.file.read(), self.foto_fmedica.content_type)
+            email.attach_file(str(settings.MEDIA_ROOT / self.foto.name))
+
+        return email
+
+    def send_mail(self, info) -> bool:
+        """Envía email donde la imagen puede estar adjunta.
+        :param info: Información enviada en el cuerpo del correo.
+        :return: True caso haber enviado el correo, False en caso contrario
+        """
+        try:
+            email = self.prepare_mail(info)
+
+            if self.foto and not email.attachments:
+                logger.error(f"{self.log_text} Perdida la referencia de imagen adjunta.")
+
+            r = email.send(fail_silently=False)
+
+        except Exception as e:
+            notify('error-email',
+                   f"ERROR ENVIANDO EMAIL- Radicado #{info['NUMERO_RADICACION']} {info['documento']}",
+                   f"JSON_DATA: {info}\n\nERROR: {e}")
+            return False
+            # TODO Revisar que hacer cuando haya error en envío de email
+            # if rad := Radicacion.objects.filter(numero_radicado=info['documento']).first():
+            # rad.delete()
+            # logger.info(f"{self.request.COOKIES.get('sessionid')[:6]} {rad}"
+            #             "Eliminado radicado al no haberse enviado correo.")
+        else:
+            if r == 1:
+                if self.foto:
+                    logger.info(f"{self.log_text} Correo enviado a {info['email']} con imagen adjunta")
+                else:
+                    logger.info(
+                        f"{self.log_text} correo enviado a {info['email']} sin imagem")
+                return True
+            else:
+                # E-mail enviado pero r != 1
+                notify('error-email', f"ERROR ENVIANDO EMAIL - Radicado #{info['documento']}",
+                       f"JSON_DATA: {info}")
+                return False
+        # finally:
+        #     if self.foto:
+        #         del_file(self.foto.file.file.name)
+
+    def make_subject_and_cco(self, info) -> tuple:
+        """
+        Create the subject and establish the CCO.
+        :param info: Dictionary with information captured from the wizard.
+                     The next keys are used: 'NOMBRE', 'documento', 'NUMERO_AUTORIZACION'
+        :return: Tuple with two elements:
+                    0 -> subject (str)
+                    1 -> copia_oculta (list)
+        """
+
+        copia_oculta = config('EMAIL_BCC', cast=Csv())
+
+        # Definiendo asunto
+        if info.get('documento'):
+            subject = f"{info['NOMBRE'].title()} - Este es el " \
+                      f"soporte de la radicación de tu domicilio en Logifarma"
+        else:
+            subject = f"{info['NUMERO_AUTORIZACION']} - Este es el " \
+                      f"número de radicación de tu domicilio en Logifarma"
+
+        if info.get('documento') in ('CC99999999',) or info.get('NUMERO_AUTORIZACION') in (
+                99_999_999, 99_999_998):
+            subject = '[OMITIR] CORREO DE PRUEBA'
+            with contextlib.suppress(Exception):
+                copia_oculta.remove('radicacion.domicilios@logifarma.co')
+
+        return subject, copia_oculta
+
+    def make_destinatary(self, info) -> list:
+        """
+        Validate if the e-mail exists.
+        If doesn\'t exists then clean the field on info_mail.
+        :param info: Dictionary with information captured from the wizard
+                    The next keys are used: 'email'
+        :return: Empty list or list with validated e-mail.
+        """
+        destinatary = []
+        # If the email key comes and his first item of the list has something, then:
+        if 'email' in info and info['email'][0]:
+            for e in info['email']:
+                if purge_email(e) and email_exists(e):
+                    destinatary.append(e)
+                else:
+                    logger.info(f'Email {e} no existe.')
+                    info['email'].remove(e)
+        return destinatary
+
