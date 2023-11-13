dbf62da800c6ef86e3ee49576463ed347d6f9503
Alfonso <alfareiza@gmail.com>
2023-11-13 11:56:43 -0300

    Excluyendo url de prueb

--- core/apps/base/urls.py
+++ core/apps/base/urls.py
@@ -6,20 +6,8 @@ from core.apps.base.views_sin_autorizacion import SinAutorizacion
 
 app_name = 'base'
 
-FORMS = [
-    ("home", Home),
-    ("autorizado_o_no", AutorizadoONo),
-    ("autorizacionServicio", AutorizacionServicio),
-    ("fotoFormulaMedica", FotoFormulaMedica),
-    ("eligeMunicipio", EligeMunicipio),
-    ("digitaDireccionBarrio", DireccionBarrio),
-    ("digitaCelular", DigitaCelular),
-    ("digitaCorreo", DigitaCorreo)
-]
-
 urlpatterns = [
     path('', ContactWizard.as_view(), name='home'),
-    path('g/', ContactWizard.as_view(FORMS), name='f'),
     path('sin-autorizacion/', SinAutorizacion.as_view(), name='sin-autorizacion'),
     path('finalizado/', finalizado, name='done'),
     path('error/', err_multitabs, name='err_multitabs')
