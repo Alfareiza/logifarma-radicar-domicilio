460841ddcd4936b953b170bf5699ffa21409e3e7
Alfonso <alfareiza@gmail.com>
2026-04-24 18:20:32 -0300

    feat: initial commit for radicacion single view

--- core/apps/home/urls.py
+++ core/apps/home/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('inicio/', views.index, name='index'),
     path('sinacta/', views.sinacta, name='sinacta'),
     path('buscador/', views.buscador, name='buscador'),
+    path('radicacion/<int:pk>/', views.radicacion_detail, name='radicacion_detail_pk'),
+    path('radicacion/<slug:ref>/', views.radicacion_detail, name='radicacion_detail'),
     path('soporte/<slug:value>', views.ver_soporte_rad, name='soporte'),
     # Authentication
     path('login/', csrf_exempt(views.UserLoginView.as_view()), name='login'),
